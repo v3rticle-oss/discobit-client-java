4db756da91b82d9390af4ed84013ebcf0baece3f
Jens Saade <jens.saade@gmail.com>
2014-09-07 22:53:50 +0200

    added agent that populates a client system context

---
+++ src/main/java/com/v3rticle/oss/discobit/client/agent/DiscobitAgent.java
@@ -0,0 +1,55 @@
+package com.v3rticle.oss.discobit.client.agent;
+
+import java.io.IOException;
+import java.lang.instrument.Instrumentation;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+import com.mashape.unirest.http.Unirest;
+import com.v3rticle.oss.discobit.client.DiscobitClientFactory;
+import com.v3rticle.oss.discobit.client.DiscobitOperationException;
+import com.v3rticle.oss.discobit.client.bootstrap.DiscobitSettings;
+
+public class DiscobitAgent {
+private static Logger log = Logger.getLogger(DiscobitAgent.class.getName());
+	
+	public static void main(String[] args){
+		execute();
+	}
+	
+	public static void premain(String agentArgs, Instrumentation inst){
+		execute();
+	}
+	
+	private static void execute(){
+		DiscobitSettings settings = new DiscobitSettings();
+		try {
+			log.info("Reading discobit configuration from " + settings.getServerURL() + "::" + settings.getDefaultConfigurationUUID());
+			Properties props = DiscobitClientFactory.getClient().getConfig(UUID.fromString(settings.getDefaultConfigurationUUID()));
+			Enumeration e = props.propertyNames();
+		    while (e.hasMoreElements()) {
+		      String key = (String) e.nextElement();
+		      String value = (String) props.getProperty(key);
+		      
+		      if (key != null && value != null){
+		    	  System.setProperty(key, value);
+		      } else if (key != null && value == null){
+		    	  System.clearProperty(key);
+		      }
+		    }
+
+			log.info("injected " + props.size() + " properties to system context");
+
+		} catch (DiscobitOperationException e) {
+			log.warning(e.getMessage());
+		} finally {
+			try {
+				Unirest.shutdown();
+			} catch (IOException e) {
+				log.warning(e.getMessage());
+			}
+		}
+	}
+}
